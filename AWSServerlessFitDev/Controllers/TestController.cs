d46ba7dcc8f40d0eb128ceca36a9e33933227421
Miron Kalversberg <mironwob@freenet.de>
2022-02-09 22:52:01 +0100

    testcontroller auskommentiert

--- AWSServerlessFitDev/Controllers/TestController.cs
+++ AWSServerlessFitDev/Controllers/TestController.cs
@@ -1,7 +1,10 @@
-﻿using AWSServerlessFitDev.Services;
+﻿using AWSServerlessFitDev.Model;
+using AWSServerlessFitDev.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +12,49 @@ using System.Threading.Tasks;
 
 namespace AWSServerlessFitDev.Controllers
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class TestController : Controller
-    {
-        //[Route("PostTestNotification")]
-        //[HttpPost]
+    //[AllowAnonymous]
+    //[Route("api/v1/[controller]")]
+    //[ApiController]
+    //public class TestController : Controller
+    //{
+        //IDatabaseService DbService { get; set; }
+        //IS3Service S3Client { get; set; }
+        //ILogger<TestController> Logger { get; set; }
+        //INotificationService NotifyService { get; set; }
+
+        //public TestController(Services.IDatabaseService dbService, IConfiguration configuration, IS3Service s3Client, INotificationService iNotifyService,
+        //    ILogger<TestController> logger)
+        //{
+        //    DbService = dbService;
+        //    NotifyService = iNotifyService;
+        //    S3Client = s3Client;
+        //    Logger = logger;
+        //}
+        //[AllowAnonymous]
+        //[Route("Test")]
+        //[HttpGet]
         //public async Task<IActionResult> PostTestNotification()
         //{
-        //    var ns = new NotificationService(null, null);
-        //    string token = "eWZCoc3SB0DZu02OMMZIaj:APA91bGOVfX1uUi88Ic6UGnKiZ2VtOzYWw5IBPMMJ1OAwp_o3R0JlKNASrZtnJU8PgPH-C45BfBanj6Pe64pFNaFLt6zJrjCW1yF1-ST9xBIMFR2CP-VPNdfQiFUeuo-rsjJ1dtwxJot";
-        //    await ns.FCMPublishMessage( token, -1, Model.NotificationType.Follow, "miron", "miron4", null, -1);
+        //    new EmailService().SendEmail("support@gymnect.de", "testsubject", "textbody");
         //    return Ok();
         //}
-    }
+        //[AllowAnonymous]
+        //[Route("Test2")]
+        //[HttpGet]
+        //public async Task<IActionResult> TestPOstCOmment()
+        //{
+        //    var comment = new PostComment()
+        //    {
+        //        IsDeleted = false,
+        //        PostId = 510,
+        //        LastModified = DateTime.UtcNow,
+        //        Text = "test",
+        //        TimePosted = DateTime.UtcNow,
+        //        UserName = "miron"
+        //    };
+        //    long? postCommentId = DbService.InsertPostComment(comment);
+        //    return Ok();
+        //}
+
+    //}
 }
